85a15766fd4619fd698db7a5b2b6fcd755dfad6d
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2026-01-25 20:51:36 -0300

    implementing post and get methods

--- api/lms/index.ts
+++ api/lms/index.ts
@@ -1,8 +1,100 @@
 import { Api } from "../../core/utils/abstract.ts";
+import { RouteError } from "../../core/utils/route-error.ts";
 import { lmsTables } from "./tables.ts";
+import { LmsQuery } from "./query.ts";
 
 export class LmsApi extends Api {
+  query = new LmsQuery(this.db);
+
+  handlers = {
+    postCourse: (req, res) => {
+      const { slug, title, description, lessons, hours } = req.body;
+      const writeResult = this.query.insertCourse({
+        slug,
+        title,
+        description,
+        lessons,
+        hours,
+      });
+
+      console.log(writeResult);
+
+      if (writeResult.changes === 0) {
+        throw new RouteError(400, "Erro ao criar curso");
+      }
+
+      res.status(201).json({
+        id: writeResult.lastInsertRowid,
+        changes: writeResult.changes,
+        title: "Curso criado.",
+      });
+    },
+
+    postLesson: (req, res) => {
+      const {
+        courseSlug,
+        slug,
+        title,
+        seconds,
+        video,
+        description,
+        order,
+        free,
+      } = req.body;
+      const writeResult = this.query.insertLesson({
+        courseSlug,
+        slug,
+        title,
+        seconds,
+        video,
+        description,
+        order,
+        free,
+      });
+
+      console.log(writeResult);
+
+      if (writeResult.changes === 0) {
+        throw new RouteError(400, "Erro ao criar aula.");
+      }
+
+      res.status(201).json({
+        id: writeResult.lastInsertRowid,
+        changes: writeResult.changes,
+        title: "aula criada.",
+      });
+    },
+
+    getCourses: (req, res) => {
+      const courses = this.query.selectCourses();
+
+      if (courses.length === 0) {
+        throw new RouteError(404, "Nenhum curso encontrado.");
+      }
+
+      res.status(200).json(courses);
+    },
+
+    getCourse: (req, res) => {
+      const { slug } = req.params;
+      const course = this.query.selectCourse(slug);
+
+      if (!course) {
+        throw new RouteError(404, "Nenhum curso encontrado.");
+      }
+
+      res.status(200).json(course);
+    },
+  } satisfies Api["handlers"];
+
   tables(): void {
     this.db.exec(lmsTables);
+  }
+
+  routes(): void {
+    this.router.post("/lms/course", this.handlers.postCourse);
+    this.router.get("/lms/courses", this.handlers.getCourses);
+    this.router.get("/lms/course/:slug", this.handlers.getCourse);
+    this.router.post("/lms/lesson", this.handlers.postLesson);
   }
 }
---
+++ api/lms/query.ts
@@ -0,0 +1,79 @@
+import { Query } from "../../core/utils/abstract.ts";
+
+type CourseData = {
+  id: number;
+  slug: string;
+  title: string;
+  description: string;
+  lessons: string;
+  hours: number;
+  created: string;
+};
+
+type LessonData = {
+  id: number;
+  course_id: number;
+  slug: string;
+  title: string;
+  seconds: number;
+  video: string;
+  description: string;
+  order: number;
+  free: number; //0/1
+  created: string;
+};
+
+type CourseCreate = Omit<CourseData, "id" | "created">;
+type LessonCreate = Omit<LessonData, "id" | "created" | "course_id"> & {
+  courseSlug: string;
+};
+
+export class LmsQuery extends Query {
+  insertCourse({ slug, title, description, lessons, hours }: CourseCreate) {
+    return this.db
+      .query(
+        /*sql*/ `
+          INSERT OR IGNORE INTO "courses"
+          ("slug", "title", "description", "lessons", "hours")
+          VALUES (?,?,?,?,?)
+        `,
+      )
+      .run(slug, title, description, lessons, hours);
+  }
+
+  insertLesson({
+    courseSlug,
+    slug,
+    title,
+    seconds,
+    video,
+    description,
+    order,
+    free,
+  }: LessonCreate) {
+    return this.db
+      .query(
+        /*sql*/ `
+          INSERT OR IGNORE INTO "lessons"
+          ("course_id", "slug", "title", "seconds", 
+            "video", "description", "order", "free")
+          VALUES ((SELECT "id" FROM "courses" WHERE "slug" = ?),?,?,?,?,?,?,?)
+        `,
+      )
+      .run(courseSlug, slug, title, seconds, video, description, order, free);
+  }
+
+  selectCourses() {
+    return this.db
+      .prepare(
+        /*sql*/ `SELECT * FROM "courses" ORDER BY "created" ASC LIMIT 100`,
+      )
+      .all() as CourseData[];
+  }
+
+  selectCourse(slug: string) {
+    return this.db
+      .prepare(/*sql*/ `SELECT * FROM "courses" WHERE "slug" = ?`)
+      .get(slug) as CourseData | undefined;
+  }
+}
