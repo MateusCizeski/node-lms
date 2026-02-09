8b8f0767f26884f3d072f9c8682b6427c5f6f059
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2026-02-09 20:54:21 -0300

    implementing security in the API

--- api/auth/services/session.ts
+++ api/auth/services/session.ts
@@ -1,15 +1,16 @@
 import { CoreProvider } from "../../../core/utils/abstract.ts";
 import { AuthQuery } from "../query.ts";
+import { randomBytesAsync } from "../utils.ts";
 
 export class SessionService extends CoreProvider {
   query = new AuthQuery(this.db);
-
   async create({ userId, ip, ua }) {
-    const sid_hash = 1;
+    const sid_hash = (await randomBytesAsync(32)).toString("base64url");
+
     const expires_ms = Date.now() + 60 * 60 * 24 * 15 * 1000;
 
-    this.query.insertSession({ sid_hash, userId, expires_ms, ip, ua });
+    this.query.insertSession({ sid_hash, expires_ms, userId, ip, ua });
 
-    return sid_hash;
+    return { sid_hash };
   }
 }
