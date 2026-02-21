c0158a1e21cb782873cdfac6f9083602ef74a43b
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2026-02-21 16:26:44 -0300

    improving validation methods

---
+++ core/utils/validate.ts
@@ -0,0 +1,88 @@
+import { RouteError } from "./route-error.ts";
+
+/** trim e não aceita string vazia.*/
+function string(x: unknown) {
+  if (typeof x !== "string") return undefined;
+
+  const s = x.trim();
+
+  if (s.length === 0) return undefined;
+
+  return x;
+}
+
+/** Se a string for number like, retorna como number*/
+function number(x: unknown) {
+  if (typeof x === "number") {
+    return Number.isFinite(x) ? x : undefined;
+  }
+
+  if (typeof x === "string" && x.trim().length !== 0) {
+    const n = Number(x);
+    return Number.isFinite(n) ? n : undefined;
+  }
+
+  return undefined;
+}
+
+/** Aceita valores como true, 'true, 1, '1' e 'on'*/
+function boolean(x: unknown) {
+  if (x === true || x === "true" || x === 1 || x === "1" || x === "on")
+    return true;
+  if (x === false || x === "false" || x === 0 || x === "0" || x === "off")
+    return false;
+  return undefined;
+}
+
+/** Verifica se é um objeto {}*/
+function object(x: unknown): Record<string, unknown> | undefined {
+  return typeof x === "object" && x !== null && !Array.isArray(x)
+    ? (x as Record<string, unknown>)
+    : undefined;
+}
+
+const email_re = /^[^@]+@[^@]+\.[^@]+$/;
+
+function email(x: unknown) {
+  const s = string(x)?.toLowerCase();
+
+  if (s === undefined) return undefined;
+
+  return email_re.test(s) ? s : undefined;
+}
+
+const password_re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/;
+/** Minimo 10 e máximo 256, pelo menos 1 caixa alta e 1 baixa e 1 digito.*/
+function password(x: unknown) {
+  if (typeof x !== "string") return undefined;
+  if (x.length < 10 || x.length > 256) return undefined;
+  return password_re.test(x) ? x : undefined;
+}
+
+type Parse<Value> = (x: unknown) => Value | unknown;
+
+function required<Value>(fn: Parse<Value>, error: string) {
+  return (x: unknown) => {
+    const value = fn(x);
+
+    if (value === undefined) throw new RouteError(422, error);
+    return value;
+  };
+}
+
+export const validate = {
+  string: required(string, "string esperada"),
+  number: required(string, "number esperada"),
+  boolean: required(string, "boolean esperada"),
+  object: required(string, "object esperada"),
+  email: required(string, "email esperada"),
+  password: required(string, "password esperada"),
+  optional: {
+    string,
+    number,
+    boolean,
+    object,
+    email,
+    password,
+  },
+};
